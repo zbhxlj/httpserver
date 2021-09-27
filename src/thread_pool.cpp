43f3780f7ac2cbd701933886fd8eeeab457cc0d8
zhubohan <2452078981@qq.com>
2021-09-27 20:45:28 +0800

    feat : add ThreadPool part but there is a bug that you can not start 2 or more TEST function in gtest, then it will coruppt. Besides that, it goes well

---
+++ src/thread_pool.cpp
@@ -0,0 +1,46 @@
+#include "thread_pool.h"
+#include "spdlog/spdlog.h"
+#include "iostream"
+
+namespace webserver{
+    ThreadPool::ThreadPool(size_t pool_size, size_t reserved_task_capacity)
+        : m_task_queue(reserved_task_capacity), m_thread_nums(pool_size), m_reserved_task_capacity(reserved_task_capacity){
+            m_threads.reserve(pool_size);
+        }
+    
+    void ThreadPool::start(){
+        m_is_running = true;
+        for(size_t i = 0; i < m_thread_nums; i++){
+            m_threads.emplace_back(std::make_unique<Thread>(
+                [this](){ this->consume_task(); })
+                );
+        }
+        spdlog::info("Start thread pool!");
+    }
+
+    void ThreadPool::stop(){
+        m_is_running = false;
+        for(auto& thread : m_threads){
+            thread->join();
+        }
+        spdlog::info("Stop thread pool!");
+    }
+
+    void ThreadPool::add_task(Task task){
+        bool ok = false;
+        while(!ok){
+            ok = m_task_queue.try_enqueue(std::move(task));
+        }
+    }
+
+    void ThreadPool::consume_task(){
+        Task task;
+        bool found = false;
+        while(m_is_running || found){
+            found = m_task_queue.try_dequeue(task);
+            if(found){
+                task();
+            }
+        }
+    }
+}
---
+++ src/thread_pool.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <vector>
+#include <memory>
+#include "thread.h"
+#include "concurrent_queue.h"
+#include "noncopyable.h"
+namespace webserver{
+
+class ThreadPool : public Noncopyable{
+public:
+    using Task = std::function<void()>;
+    ThreadPool(size_t thread_nums, size_t reserved_task_nums);
+    void start();
+    void stop();
+    
+    void add_task(Task task);
+
+private:
+    void consume_task();
+    std::vector<std::unique_ptr<Thread>> m_threads;
+    moodycamel::ConcurrentQueue<Task> m_task_queue;
+    std::atomic<bool> m_is_running;
+    size_t m_thread_nums;
+    size_t m_reserved_task_capacity;
+    
+};
+
+}
